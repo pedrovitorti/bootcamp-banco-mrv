1035be18833e3e4630d4c9896a4d0ed2fd0203d0
Pedro Vitor <pedrovitorti@gmail.com>
2021-06-01 12:11:30 -0300

    Exemplo prático com c# console

---
+++ Aluno.cs
@@ -0,0 +1,8 @@
+namespace Revisao
+{
+    public struct Aluno { //poderia ser uma classe, usada so para o exemplo
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+    }
+
+}
